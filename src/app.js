17cd1f8810912a112e0590e0cb418fef3efffcc1
Alexander Oyolo <alexander.oyolo@imediagroup360.com>
2025-11-17 15:54:22 -0500

    add_enviroment

--- src/app.js
+++ src/app.js
@@ -2,7 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // dev
+      "https://dashboardtifronted.vercel.app", // prod
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes.js");
