0c3460633bbced576de30290b39f0650aea30d5b
Alexander Oyolo <alexander.oyolo@imediagroup360.com>
2025-11-17 12:03:33 -0500

    update_port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const sequelize = require("./config/db");
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 sequelize
   .authenticate()
